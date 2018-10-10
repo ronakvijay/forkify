26b9a05c4a8ea1d18034ea47cb7356e907de7ac3
Ronak Vijay <ronaksvijay@gmail.com>
2018-10-10 19:43:51 +0530

    api integrated

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /* Global state of the app
  *- Search Object
@@ -10,24 +12,27 @@ const state = {};
 
 const controlSearch = async () => {
     // 1) get the query from the view
-    const query = 'pizza' //TODO
+    const query = searchView.getInput();
 
     if (query) {
         // 2) New search object and add to state
         state.search = new Search(query);
 
         // 3) Prepare UI for results
-        
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
         // 4) Search for recipes
         await state.search.getResults();
 
         // 5) Render results on UI
-        console.log(state.search.result);
+        clearLoader();
+        searchView.renderResults(state.search.result);
         
     }
 }
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
-})
+});
 
